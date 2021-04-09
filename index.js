28f0128335eef3b14b61e3c38e5c4c85f802c8ff
wuhuiluo <510426343@qq.com>
2021-04-09 17:01:54 +0800

    latest commit

--- index.js
+++ index.js
@@ -49,17 +49,139 @@ const arr = [4, 11, 5, 79, 454, 67, 31]
 
 // console.log(insertSort(arr));
 
-function quickSort(arr) {
-    if (arr.length <= 1) return arr
-    let middleIndex = Math.floor(arr.length / 2)
-    let middleValue = arr.splice(middleIndex, 1)[0]
-    let arrLeft = []
-    let arrRight = []
-    for (let i = 0; i < arr.length; i++) {
-        let item = arr[i]
-        middleValue > item ? arrLeft.push(item) : arrRight.push(item)
-    }
-    return quickSort(arrLeft).concat(middleValue, quickSort(arrRight))
-}
+// function quickSort(arr) {
+//     if (arr.length <= 1) return arr
+//     let middleIndex = Math.floor(arr.length / 2)
+//     let middleValue = arr.splice(middleIndex, 1)[0]
+//     let arrLeft = []
+//     let arrRight = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let item = arr[i]
+//         middleValue > item ? arrLeft.push(item) : arrRight.push(item)
+//     }
+//     return quickSort(arrLeft).concat(middleValue, quickSort(arrRight))
+// }
 
-console.log(quickSort(arr));
+// console.log(quickSort(arr));
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log('promise1');
+// })
+
+// console.log('1', promise1);
+
+// const promise = new Promise((resolve, reject) => {
+//     console.log(1);
+//     resolve('success')
+//     console.log(2);
+// })
+
+// promise.then(() => {
+//     console.log(3);
+// })
+// console.log(4);
+
+// const promise = new Promise((resolve, reject) => {
+//     console.log(1);
+//     console.log(2);
+// })
+
+// promise.then(() => {
+//     console.log(3);
+// })
+
+// console.log(4);
+
+// const promise1 = new Promise((resolve, reject) => {
+//     console.log('promise1');
+//     resolve('resolve1')
+// })
+
+// const promise2 = promise1.then(res => {
+//     console.log(res);
+// })
+
+// console.log('1', promise1);
+// console.log('2', promise2);
+
+// const fn = () => (new Promise((resolve, reject) => {
+//     console.log(1);
+//     console.log('success');
+// }))
+
+// console.log('start');
+// fn().then(res => {
+//     console.log(res);
+// })
+
+// console.log('start')
+// setTimeout(() => {
+//     console.log('time');
+// })
+
+// Promise.resolve().then(() => {
+//     console.log('resolve');
+// })
+
+// console.log('end');
+
+// const promise = new Promise((resolve, reject) => {
+//     console.log(1); // 1
+//     setTimeout(() => { // 放到下一个宏任务的延迟队列中
+//         console.log('timeStart'); // 4
+//         resolve('success')
+//         console.log('timeEnd'); // 5
+//     }, 0)
+//     console.log(2); // 2
+// })
+
+// promise.then((res) => { // pending -> resolve
+//     console.log(res); // 6
+// })
+// console.log(4); // 3
+
+// setTimeout(() => {
+//     console.log('timer1');
+//     Promise.resolve().then(() => {
+//         console.log('1');
+//     })
+// }, 0)
+// setTimeout(() => {
+//     console.log('timer2')
+// }, 0)
+// console.log('start')
+
+
+// const promise1 = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('success')
+//     }, 1000)
+// })
+// const promise2 = promise1.then(() => {
+//     throw new Error('error!!!')
+// })
+// console.log('promise1', promise1)
+// console.log('promise2', promise2)
+// setTimeout(() => {
+//     console.log('promise1', promise1)
+//     console.log('promise2', promise2)
+// }, 2000)
+
+
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("success");
+        console.log("timer1");
+    }, 1000);
+    console.log("promise1里的内容");
+});
+const promise2 = promise1.then(() => {
+    throw new Error("error!!!");
+});
+console.log("promise1", promise1);
+console.log("promise2", promise2);
+setTimeout(() => {
+    console.log("timer2");
+    console.log("promise1", promise1);
+    console.log("promise2", promise2);
+}, 2000);
