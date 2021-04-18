9094fc3dafec79e82d810cfd2a9abfadf365e3f3
wuhuiluo <510426343@qq.com>
2021-04-18 14:44:08 +0800

    latest commit

--- index.js
+++ index.js
@@ -1231,15 +1231,50 @@
 //     return debounced
 // }
 
-function throttle(fn, wait) {
-    let previous = 0
-    return function () {
-        let context = this
-        let args = arguments
-        let now = +new Date()
-        if (now - previous > 0) {
-            fn.apply(context, args)
-            previous = now
-        }
-    }
+// function throttle(fn, wait) {
+//     let previous = 0
+//     return function () {
+//         let context = this
+//         let args = arguments
+//         let now = +new Date()
+//         if (now - previous > 0) {
+//             fn.apply(context, args)
+//             previous = now
+//         }
+//     }
+// }
+
+// function throttle(fn, wait) {
+//     let flag = true
+//     return function () {
+//         let context = this
+//         let args = arguments
+//         if (!flag) return
+//         flag = false
+//         setTimeout(function () {
+//             fn.apply(context, args)
+//             flag = true
+//         }, wait)
+//     }
+// }
+
+Function.prototype.myCall = function (thisArg, ...args) {
+    const fn = Symbol('fn') // 声明一个独有的Symbol属性, 防止fn覆盖已有属性 
+    thisArg = thisArg || window // 若没有传入this, 默认绑定window对象
+    thisArg[fn] = this // this指向调用call的对象,即我们要改变this指向的函数
+    const result = thisArg[fn](...args) // 执行当前函数
+    delete thisArg[fn] // 删除我们声明的fn属性
+    return result // 返回函数执行结果
 }
+
+//变更函数调用者示例
+function foo() {
+    console.log(this.name)
+}
+
+// 测试
+const obj = {
+    name: '写代码像蔡徐抻'
+}
+obj.foo = foo // 变更foo的调用者
+obj.foo() // '写代码像蔡徐抻'
