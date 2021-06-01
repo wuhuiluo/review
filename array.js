9f1b3df2d528863eb2fbe6e9cc453f5a6f9a38f6
wuhuiluo <510426343@qq.com>
2021-06-01 14:15:40 +0800

    latest commit

--- array.js
+++ array.js
@@ -146,3 +146,129 @@
 // })
 
 // console.log(a);
+
+// toString()将数组中的元素用逗号拼接成字符串(不改变原数组)
+// let a = [1, 2, 3, 4, 5]
+// let result = a.toString()
+// console.log(result === '1,2,3,4,5');
+// console.log(a);
+
+// indexOf从索引为0开始，检查数组中是否包含value，有则返回匹配到的第一个索引，没有返回-1(不改变原数组)
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.indexOf(2)
+// console.log(result);
+// console.log(a);
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.indexOf(6)
+// console.log(result);
+// console.log(a);
+
+// lastIndexOf(value)从最后的索引开始，检查数组是否包含value，有则返回匹配到的第一个索引,否则返回-1
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.lastIndexOf(1)
+// console.log(result);
+// console.log(a);
+
+// concat(value)将数组和/或值连接成新数组(不改变原数组)
+
+// let a = [1, 2],
+//     b = [3, 4],
+//     c = 5
+// let result = a.concat(b, c)
+// console.log(result);
+// console.log(a);
+
+// fill(value,start,end)使用给定的value填充数组，从索引start开始end结束，不包含end(不改变数组)
+
+// flat()将二维数组变成一维数组(不改变数组元素)
+// let a = [1, 2, 3, [4, 5]]
+// let result = a.flat()
+// console.log(result);
+// console.log(a);
+
+// forEach 遍历数组(不改变原数组)
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.forEach((item, i) => {
+//     console.log(item, i);
+// })
+// console.log(result);
+// console.log(a);
+
+// every(fn) 判断数组中是否所有元素满足fn函数中的条件（不改变原数组）
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.every(currentIndex => {
+//     return currentIndex > 0
+// })
+// console.log(result);
+// console.log(a);
+
+// filter(fn)返回数组中满足fn函数中条件的集合（不改变原数组）
+// let a = [1, 2, 3, 4, 5]
+// let result = a.filter(currentIndex => {
+//     return currentIndex > 4
+// })
+// console.log(result);
+// console.log(a);
+
+// find(fn)返回数组中第一个匹配fn函数中条件的值没有则返回undefined（不改变原数组）
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.find(currentIndex => {
+//     return currentIndex > 6
+// })
+// console.log(result)
+// console.log(a);
+
+// findIndex(fn)返回数组中第一个匹配fn函数中条件的索引没有则返回undefined(不改变原数组)
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.findIndex(currentIndex => {
+//     return currentIndex > 3
+// })
+// console.log(result);
+// console.log(a);
+
+// includes()返回一个布尔值，表示数组是否包含给定的值(不改变原数组)
+// let a = [1, 2, 3, 4, 5]
+// let result = a.includes(2)
+// console.log(result);
+// console.log(a);
+
+// map(fn)以fn函数中返回值组成新的数组返回(不改变原数组)
+
+// let a = [1, 2, 3, 4, 5]
+// let result = a.map((v, i) => {
+//     return 9
+// })
+
+// console.log(result);
+// console.log(a);
+
+
+// reduce()计数器(不改变原数组)
+
+// let a = [1, 2, 3, 4, 5]
+// // let result = a.reduce((prev, cur) => {
+// //     return prev + cur
+// // }, 5)
+
+// // console.log(result);
+// // console.log(a);
+
+// let result = a.reduce((prev, cur) => {
+//     return prev + cur
+// })
+// console.log(result);
+
+// some(fn)检查数组中是否含有满足fn函数条件的值(不改变原数组)
+// let a = [1, 2, 3, 4, 5]
+// let result = a.some(v => {
+//     return v > 2
+// })
+// console.log(result);
+// console.log(a);
